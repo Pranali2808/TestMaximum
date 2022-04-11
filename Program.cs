5659ea87e9b96426929532e7ad07f4b1ebff1799
pranali2808 <ranepranali1997@gmail.com>
2022-04-11 17:13:32 +0530

    find the maximum from given theree integers

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Testmaximum  program");
+
+           
+               Console.ReadLine();
+        }
+    }
+}
