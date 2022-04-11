ec16eb522f85c1ebaa3480040f06e5a035a3c541
pranali2808 <ranepranali1997@gmail.com>
2022-04-11 17:23:47 +0530

    find the maximum from 3 integers

--- Program.cs
+++ Program.cs
@@ -11,9 +11,33 @@ namespace TestMaximum
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Testmaximum  program");
-
-           
-               Console.ReadLine();
+            TestMax maximum = new TestMax();
+            Console.WriteLine("Select from below \n1. Find Max Of Three Int Number\n ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter first integer");
+                    int intNum1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter second integer");
+                    int intNum2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter third integer");
+                    int intNum3 = Convert.ToInt32(Console.ReadLine());
+                    int intMax = maximum.MaxInThree(intNum1, intNum2, intNum3);
+                    if (intMax == 0)
+                    {
+                        Console.WriteLine("Two or Three numbers have equal value");
+                    }
+                    else
+                        Console.WriteLine("Maximum out of given numbers {0} {1} & {2} is {3}", intNum1, intNum2, intNum3, intMax);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+                    Console.ReadLine();
+            
         }
+        
     }
 }
